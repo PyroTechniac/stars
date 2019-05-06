1803feee4bf8e4d7051ce210c324806db5e9ac3a
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-05-06 15:16:25 -0400

    switch to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,11 @@ const gulp = require('gulp');
 const gulp_ts = require('gulp-typescript');
 const gulp_sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
-const path = require('path');
-const { execSync } = require('child_process');
 const lib = 'lib/'
 
 const project = gulp_ts.createProject('tsconfig.json');
 
-gulp.task('build', () => {
+const build = () => {
     del.sync(['bin/**/*.*'])
     const tsCompile = gulp.src('src/**/*.ts')
         .pipe(gulp_sourcemaps.init({ base: 'src' }))
@@ -25,11 +23,6 @@ gulp.task('build', () => {
     return tsCompile.js
         .pipe(gulp_sourcemaps.write('.', { sourceRoot: '../src' }))
         .pipe(gulp.dest(lib))
-})
+}
 
-gulp.task('build:scripts', () => {
-    del.sync(['scripts/**/*.js'])
-    gulp.src('scripts/**/*.ts')
-        .pipe(project())
-        .pipe(gulp.dest('scripts/'))
-})
+exports.build = build;
